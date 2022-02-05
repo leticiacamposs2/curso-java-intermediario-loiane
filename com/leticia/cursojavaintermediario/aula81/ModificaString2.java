1c00a919db70bd3a39631711e0e8821ead4225ae
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-02-05 01:29:00 -0300

    modifica string 2

---
+++ com/leticia/cursojavaintermediario/aula81/ModificaString2.java
@@ -0,0 +1,16 @@
+package com.leticia.cursojavaintermediario.aula81;
+
+public class ModificaString2 {
+
+	public static void main(String[] args) {
+		
+		String teste = "Teste";
+		
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);		
+	}
+
+}
